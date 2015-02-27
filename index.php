c5f9eadbed48c54a61c6506dfbb51cb92efdea37
Amy Norman <amskittle@gmail.com>
2015-02-27 12:16:00 -0700

    Changed index, added header module

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Amy's Sandbox - CIT261 </title>
+		<meta name="Author" content="Amy Norman">
+		<script type="text/javascript">
+
+		</script>
+	</head>
+	<body>
+		<header>
+			<?php include("/mod/header.html"); ?>
+		</header>
+
+		<h1> Amy's Sandbox - CIT 261 </h1>
+		<p>Welcome to my sandbox! This is where I will be keeping navigation to each of my working examples. For this class, I will be exploring 7 different technologies:</p>
+		<ul>
+			<li>JavaScript Objects</li>
+			<li>DOM manipulation</li>
+			<li>CSS3 transitions</li>
+			<li>CSS3 animations</li>
+			<li>Using local data storage</li>
+			<li>AJAX interactions with remote services</li>
+			<li>JSON data serialization</li>
+		</ul>
+
+
+		<section>
+			<h2> Basic Examples </h2>
+			<p>These examples are meant to showcase the individual technologies that we are covering in this class. They focus on the one technology, but can grow as the semester goes on. </p>
+			
+			<h3>JavaScript Objects</h3>
+			<a href="http://norman-amy.github.io/Amys-Sandbox/JavaScript-Objects/js-object-example.html" title="JavaScript Object Example 1">Object Example 1</a><br>
+			<a href="http://norman-amy.github.io/Amys-Sandbox/JavaScript-Objects/js-object-inheritance-example.html" title="JavaScript Object Inheritance Example">Inheritance Example</a><br>
+
+			<h3>DOM Manipulation</h3>
+			<a href="" title=""></a><br>
+
+			<h3>CSS3 Transitions</h3>
+			<a href="http://norman-amy.github.io/Amys-Sandbox/CSS3-Transforms/css3-transforms.html﻿" title="CSS3 Transitions">CSS Transitions</a><br>
+
+			<h3>CSS3 Animations</h3>
+			<a href="http://norman-amy.github.io/Amys-Sandbox/CSS3-Animations/css-animations-example.html" title="CSS Animations">CSS Animations</a><br>
+
+			<h3>Local Data Storage</h3>
+			<a href="" title=""></a><br>
+
+			<h3>AJAX</h3>
+			<a href="http://norman-amy.github.io/Amys-Sandbox/AJAX/ajax-basic-example.html" title="Basic AJAX Example">AJAX - Basic</a><br>
+			<a href="http://norman-amy.github.io/Amys-Sandbox/jQuery/jQuery-example.html" title="AJAX using jQuery">AJAX using jQuery</a><br>
+
+			<h3>JSON Serialization</h3>
+			<a href="http://norman-amy.github.io/Amys-Sandbox/JavaScript-Objects/js-object-serialization-example.html" title="JavaScript Object (JSON) Serialization">JavaScript Object (JSON) Serialization</a><br>
+
+		</section>
+		<section>
+			<h2> Combination Examples </h2>
+			<p>These more complicated examples showcase multiple technologies working together.</p>
+			<ul>
+				<li><a href="http://norman-amy.github.io/Amys-Sandbox/AJAX/ajax-json-example.html" title="AJAX, JSON, and Local Storage">AJAX JSON and Local Storage</a></li>
+			</ul>
+		</section>
+		<section>
+			<h2> Apps </h2>
+			<p>These apps use every technology to accomplish a task.</p>
+			<ul>
+				<li><a href="http://norman-amy.github.io/Amys-Sandbox/Combination/who-are-you.html" title="Mood Tracker App">Mood Tracker</a></li>
+			</ul>
+	</section>
+	</body>
+</html>
